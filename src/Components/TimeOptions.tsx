7d153a21695a7d4bd045ad52a35c8ba6ff413d68
melissamelissaa <ninichiaberashvili2002@gmail.com>
2023-05-12 13:28:07 +0400

    Ameliorate style, add exit button

--- src/Components/TimeOptions.tsx
+++ src/Components/TimeOptions.tsx
@@ -8,50 +8,56 @@ type timeOptionsProps = {
   secValue: Function;
   handleClick: () => void;
   clicked: boolean;
-  
 };
 
 export const TimeOptions = (props: timeOptionsProps) => {
   if (!props.clicked) {
     return (
-      <div className="timeOptions-div">
-        <FontAwesomeIcon icon={faTimes} className="icon" onClick={props.handleClick}/>
-        <label className="label">DAYS</label>
-        <input
-          required
-          className="input"
-          type="number"
-          placeholder="0"
-          onChange={(e) => props.dayValue(e.target.value)}
+      
+        <div className="timeOptions-div">
+
+        <FontAwesomeIcon
+          icon={faTimes}
+          className="icon exit"
+          onClick={props.handleClick}
         />
-        <label className="label">HOURS</label>
-        <input
-          required
-          className="input"
-          type="number"
-          placeholder="0"
-          onChange={(e) => props.hourValue(e.target.value)}
-        />
-        <label className="label">MINUTES</label>
-        <input
-          required
-          className="input"
-          type="number"
-          placeholder="0"
-          onChange={(e) => props.minValue(e.target.value)}
-        />
-        <label className="label">SECONDS</label>
-        <input
-          required
-          className="input"
-          type="number"
-          placeholder="0"
-          onChange={(e) => props.secValue(e.target.value)}
-        />
-        <button className="timeOptions-btn" onClick={props.handleClick}>
-          Start
-        </button>
-      </div>
+          <label className="label">DAYS</label>
+          <input
+            required
+            className="input"
+            type="number"
+            placeholder="0"
+            onChange={(e) => props.dayValue(e.target.value)}
+          />
+          <label className="label">HOURS</label>
+          <input
+            required
+            className="input"
+            type="number"
+            placeholder="0"
+            onChange={(e) => props.hourValue(e.target.value)}
+          />
+          <label className="label">MINUTES</label>
+          <input
+            required
+            className="input"
+            type="number"
+            placeholder="0"
+            onChange={(e) => props.minValue(e.target.value)}
+          />
+          <label className="label">SECONDS</label>
+          <input
+            required
+            className="input"
+            type="number"
+            placeholder="0"
+            onChange={(e) => props.secValue(e.target.value)}
+          />
+          <button className="timeOptions-btn" onClick={props.handleClick}>
+            Start
+          </button>
+        </div>
+
     );
   } else return null;
 };
